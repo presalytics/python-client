125598084b61e6a9715a21242710485974d47304
Kevin Hannegan <kevin@chart-a-lot.com>
2019-10-04 03:09:42 +0000

    fix ApiClient.__init__ argmuents

--- presalytics/client/api.py
+++ presalytics/client/api.py
@@ -7,19 +7,19 @@ from .auth import AuthenticationMixIn
 class DocConverterApiClientWithAuth(AuthenticationMixIn, presalytics_doc_converter.api_client.ApiClient):
     def __init__(self, **kwargs):
         AuthenticationMixIn.__init__(self, **kwargs)
-        presalytics_doc_converter.api_client.ApiClient.__init__(self, **kwargs)
+        presalytics_doc_converter.api_client.ApiClient.__init__(self)
         self.update_configuration()
 
 class OoxmlAutomationApiClientWithAuth(AuthenticationMixIn, presalytics_ooxml_automation.api_client.ApiClient):
     def __init__(self, **kwargs):
         AuthenticationMixIn.__init__(self, **kwargs)
-        presalytics_ooxml_automation.api_client.ApiClient.__init__(self, **kwargs)
+        presalytics_ooxml_automation.api_client.ApiClient.__init__(self)
         self.update_configuration()
 
 class StoryApiClientWithAuth(AuthenticationMixIn, presalytics_story.api_client.ApiClient):
     def __init__(self, **kwargs):
         AuthenticationMixIn.__init__(self, **kwargs)
-        presalytics_ooxml_automation.api_client.ApiClient.__init__(self, **kwargs)
+        presalytics_ooxml_automation.api_client.ApiClient.__init__(self)
         self.update_configuration()
 
 

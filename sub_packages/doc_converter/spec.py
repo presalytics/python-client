c475590335ffb0415234485960afe6f54dda3a6e
Kevin Hannegan <kevin@chart-a-lot.com>
2019-12-31 18:32:20 +0000

    add subpackages

---
+++ sub_packages/doc_converter/spec.py
@@ -0,0 +1,18 @@
+import os
+from environs import Env
+
+env = Env()
+env.read_env()
+
+doc_converter_host = env("DOC_CONVERTER_HOST", "https://api.presalytics.io")
+
+SPEC = {
+        "update": False,
+        "update_type": "patch",
+        "endpoint": "{}/doc-converter/openapi.json.".format(doc_converter_host),
+        "package_name": "presalytics_doc_converter",
+        "package_url": "https://github.com/presalytics/doc-converter-python-client.git",
+        "readme_path": os.path.join(os.path.dirname(os.path.realpath(__file__)), "README.md"),
+        "setuppy_path": os.path.join(os.path.dirname(os.path.realpath(__file__)), "setup.py"),
+        "pypi_url": "https://pypi.org/project/presalytics-doc-converter/"
+}

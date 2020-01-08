01b3b30d52e43732e1ae3a00372ec05ca2aa3eec
Kevin Hannegan <kevin@chart-a-lot.com>
2020-01-08 00:13:02 +0000

    initilaize reveal at window.onload

--- presalytics/lib/plugins/reveal.py
+++ presalytics/lib/plugins/reveal.py
@@ -30,7 +30,7 @@ class RevealConfigPlugin(ext.ScriptPlugin, jinja.JinjaPluginMakerMixin):
         config.update({'reveal_config': reveal_config})
         return self.render(config)
 
-    template = '<script type="text/javascript">window.onload = function(e){Reveal.initialize({{ reveal_config|tojson(indent=4) }});};</script>'
+    template = '<script type="text/javascript"> window.onload = function(e){Reveal.initialize({{ reveal_config|tojson(indent=4) }});};</script>'
 
     default_config = {
         'controls': True,  # Display presentation control arrows

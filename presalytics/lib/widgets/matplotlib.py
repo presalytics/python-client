9755a5b888288c97fea5ae538cd6ac2e52a0bfaa
Kevin Hannegan <kevin@chart-a-lot.com>
2020-02-26 00:56:32 +0000

    change matplotlib figure

--- presalytics/lib/widgets/matplotlib.py
+++ presalytics/lib/widgets/matplotlib.py
@@ -233,9 +233,9 @@ class MatplotlibResponsiveFigure(MatplotlibFigure):
                 <script type="text/javascript" src="{{ mpld3_url }}"></script>
                 <div id={{ figid }}></div>
                 <script type="text/javascript">
-                    $(document).ready(function() {
-                        mpld3.draw_figure({{ figid }}, {{ figure_json }});
-                    });
+
+                    window.onload = mpld3.draw_figure({{ figid }}, {{ figure_json }});
+            
                 </script>
             </body>
         </html>""")

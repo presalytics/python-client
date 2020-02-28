5f9da5c3ae991e3938911774e6561e4b9f2fed4a
Kevin Hannegan <kevin@chart-a-lot.com>
2020-02-28 23:40:14 +0000

    fix typo

--- presalytics/lib/widgets/matplotlib.py
+++ presalytics/lib/widgets/matplotlib.py
@@ -185,7 +185,7 @@ class MatplotlibResponsiveFigure(MatplotlibFigure):
             'src': source_url,
             'frameborder': "0",
             'scrolling': "no",
-            'class': 'matplotlib-responsive-frame'
+            'class': 'matplotlib-responsive-frame',
             'style': 'max-height: none; max-width: none;'
         })
         empty_parent_div.append(frame)
